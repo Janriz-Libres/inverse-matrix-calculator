ca20a0426ee5ea00b7492b02cc8dccd802d0f2e2
Janriz Libres <libres.janriz@gmail.com>
2023-11-27 22:45:54 +0800

    implemented fraction representation

--- src/main/java/com/teagang/projecttea/DFraction.java
+++ src/main/java/com/teagang/projecttea/DFraction.java
@@ -1,30 +1,100 @@
 package com.teagang.projecttea;
 
-import org.apache.commons.math3.fraction.Fraction;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class DFraction {
+    private BigDecimal num, den;
 
-    double num, den;
+    public static final DFraction ZERO = new DFraction("0", "1");
+    public static final DFraction ONE = new DFraction("1", "1");
 
-    public DFraction(double num, double den) {
-        this.num = num;
-        this.den = den;
+    public DFraction(String num, String den) {
+        this.num = new BigDecimal(num);
+        this.den = new BigDecimal(den);
+
+        boolean numIsPos = this.num.compareTo(BigDecimal.ZERO) >= 0;
+        boolean denIsPos = this.den.compareTo(BigDecimal.ZERO) >= 0;
+
+        if (numIsPos == denIsPos) {
+            this.num = this.num.abs();
+        } else {
+            this.num = this.num.abs().negate();
+        }
+
+        this.den = this.den.abs();
+        reduce();
+    }
+
+    public boolean isZero() {
+        return this.num.compareTo(BigDecimal.ZERO) == 0;
+    }
+
+    private BigDecimal gcd(BigDecimal a, BigDecimal b) {
+        if (b.compareTo(BigDecimal.ZERO) == 0) {
+            return a;
+        }
+        return gcd(b, a.remainder(b));
+    }
+
+    private void reduce() {
+        if (this.num.equals(BigDecimal.ZERO) || this.den.equals(BigDecimal.ONE)) {
+            return;
+        }
+
+        try {
+            int a = this.num.intValueExact();
+            int b = this.den.intValueExact();
+        } catch (ArithmeticException e) {
+            this.num = this.num.divide(this.den, MathContext.DECIMAL128);
+            this.den = BigDecimal.ONE;
+            return;
+        }
+
+        BigDecimal gcdVal = gcd(this.num.abs(), this.den);
+        this.num = this.num.divide(gcdVal, MathContext.DECIMAL128);
+        this.den = this.den.divide(gcdVal, MathContext.DECIMAL128);
     }
 
     public DFraction multiply(DFraction df) {
-        double newNum = this.num * df.num;
-        double newDen = this.den * df.den;
+        String newNum = this.num.multiply(df.num).toPlainString();
+        String newDen = this.den.multiply(df.den).toPlainString();
         return new DFraction(newNum, newDen);
     }
 
     public DFraction divide(DFraction df) {
-        double newNum = this.num * df.den;
-        double newDen = this.den * df.num;
+        String newNum = this.num.multiply(df.den).toPlainString();
+        String newDen = this.den.multiply(df.num).toPlainString();
         return new DFraction(newNum, newDen);
     }
 
     public DFraction minus(DFraction df) {
-        Fraction frac = new Fraction(9, 9);
-        return new DFraction(9, 9); //Placeholder
+        BigDecimal op1 = this.num.multiply(df.den);
+        BigDecimal op2 = this.den.multiply(df.num);
+        String newNum = op1.subtract(op2).toPlainString();
+        String newDen = this.den.multiply(df.den).toPlainString();
+        return new DFraction(newNum, newDen);
+    }
+
+    @Override
+    public String toString() {
+        if (this.num.equals(BigDecimal.ZERO))
+            return "0";
+
+        if (this.den.equals(BigDecimal.ONE))
+            return this.num.toString();
+
+        try {
+            int a = this.num.intValueExact();
+
+            if (Integer.toString(a).toLowerCase().contains("e") || this.den.toString().toLowerCase().contains("e")
+                    || this.num.toPlainString().length() > 7 || this.den.toPlainString().length() > 7) {
+                throw new ArithmeticException();
+            }
+        } catch (ArithmeticException e) {
+            return this.num.divide(this.den, MathContext.DECIMAL128).toString();
+        }
+
+        return this.num.toPlainString() + "/" + this.den.toPlainString();
     }
 }
--- src/main/java/com/teagang/projecttea/Solver.java
+++ src/main/java/com/teagang/projecttea/Solver.java
@@ -1,6 +1,5 @@
 package com.teagang.projecttea;
 
-import org.apache.commons.math3.fraction.Fraction;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
@@ -32,52 +31,84 @@ public class Solver {
         return result;
     }
 
-    private String[][] augmentMatrix(String[][] matrix) {
-        int n = matrix.length;
-        String[][] augmentedMatrix = new String[n][2 * n];
+    private DFraction[][] parseFinalData(String[][] data) {
+        int n = data.length;
+        DFraction[][] result = new DFraction[n][n];
 
         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (data[i][j].contains("/")) {
+                    String[] part = data[i][j].split("/");
+                    result[i][j] = new DFraction(part[0], part[1]);
+                    continue;
+                }
+
+                result[i][j] = new DFraction(data[i][j], "1");
+            }
+        }
+
+        return result;
+    }
+
+    private DFraction[][] augmentMatrix(DFraction[][] matrix) {
+        int n = matrix.length;
+        DFraction[][] augmentedMatrix = new DFraction[n][2 * n];
+
+        // Augment with identity matrix
+        for (int i = 0; i < n; i++) {
             System.arraycopy(matrix[i], 0, augmentedMatrix[i], 0, n);
-            augmentedMatrix[i][i + n] = "1"; // Augment with identity matrix
+
+            for (int j = 0; j < n; j++) {
+                augmentedMatrix[i][j + n] = (i == j) ? DFraction.ONE : DFraction.ZERO;
+            }
         }
 
         return augmentedMatrix;
     }
 
-    private double[][] invertMatrix(String[][] matrix) {
+    private String[][] invertMatrix(DFraction[][] matrix) {
         int n = matrix.length;
-        String[][] augmentedMatrix = augmentMatrix(matrix);
+        DFraction[][] augmentedMatrix = augmentMatrix(matrix);
 
         // Apply Gaussian elimination to get row-echelon form
         for (int i = 0; i < n; i++) {
-            // Make the diagonal element 1
-            if (augmentedMatrix[i][i].contains("/")) {
-                String[] parts = augmentedMatrix[i][i].split("/");
-                int num = Integer.parseInt(parts[0]);
-                int den = Integer.parseInt(parts[1]);
-                Fraction fraction = new Fraction(num, den);
+            // Find pivot
+            if (augmentedMatrix[i][i].isZero()) {
+                for (int j = i + 1; j < n; j++) {
+                    if (augmentedMatrix[j][i].isZero())
+                        continue;
+
+                    DFraction[] temp = new DFraction[2 * n - i];
+                    System.arraycopy(augmentedMatrix[i], i, temp, 0, 2 * n - i);
+                    System.arraycopy(augmentedMatrix[j], i, augmentedMatrix[i], i, 2 * n - i);
+                    System.arraycopy(temp, 0, augmentedMatrix[j], i, 2 * n - i);
+                }
             }
 
-//            double diagonalElement = augmentedMatrix[i][i];
-            for (int j = 0; j < 2 * n; j++) {
-//                augmentedMatrix[i][j] /= diagonalElement;
+            // Make the diagonal element 1
+            DFraction diagonalElement = augmentedMatrix[i][i];
+            for (int j = i; j < 2 * n; j++) {
+                augmentedMatrix[i][j] = augmentedMatrix[i][j].divide(diagonalElement);
             }
 
             // Make the elements above and below the diagonal 0
             for (int k = 0; k < n; k++) {
-                if (k != i) {
-//                    double factor = augmentedMatrix[k][i];
-                    for (int j = 0; j < 2 * n; j++) {
-//                        augmentedMatrix[k][j] -= factor * augmentedMatrix[i][j];
+                if (k != i && !augmentedMatrix[k][i].isZero()) {
+                    DFraction factor = augmentedMatrix[k][i];
+                    for (int j = i; j < 2 * n; j++) {
+                        DFraction operand = factor.multiply(augmentedMatrix[i][j]);
+                        augmentedMatrix[k][j] = augmentedMatrix[k][j].minus(operand);
                     }
                 }
             }
         }
 
         // Extract the inverted matrix
-        double[][] invertedMatrix = new double[n][n];
+        String[][] invertedMatrix = new String[n][n];
         for (int i = 0; i < n; i++) {
-            System.arraycopy(augmentedMatrix[i], n, invertedMatrix[i], 0, n);
+            for (int j = 0; j < n; j++) {
+                invertedMatrix[i][j] = augmentedMatrix[i][j + n].toString();
+            }
         }
 
         return invertedMatrix;
@@ -91,21 +122,7 @@ public class Solver {
             throw new SingularMatrixException();
         }
 
-        double[][] inverse = invertMatrix(data);
-        String[][] result = new String[data.length][data[0].length];
-
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[i].length; j++) {
-                Fraction fraction = new Fraction(inverse[i][j]);
-
-                if (fraction.doubleValue() == inverse[i][j] && fraction.doubleValue() != 0) {
-                    result[i][j] = fraction.toString();
-                } else {
-                    result[i][j] = Double.toString(inverse[i][j]);
-                }
-            }
-        }
-
-        return result;
+        DFraction[][] finalParsedData = parseFinalData(data);
+        return invertMatrix(finalParsedData);
     }
 }
